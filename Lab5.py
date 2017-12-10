6993c4f83a412d6fff68aecfef9a200cf5b4d915
Anatoly Makarevich <anatoly_mak@yahoo.com>
2017-12-10 22:55:36 +0300

    Changed Lab5/"Lab18" to be a self-contained object.

--- Lab5.py
+++ Lab5.py
@@ -22,103 +22,127 @@ def P(*args):
     if debug: print(*args)
     pass
 
-gen_times = []
-solve_times = []
 
-class Terminal(object):
+class Lab18(object):
 
-    def __init__(self, env, resource, t=25, mu=20, name=""):
-        self.env = env
-        self.resource = resource
-        self.action = env.process(self.run())
-        self.t = t
-        self.mu = mu
-        self.name = name
-        pass
+    class Terminal(object):
+        """Represents a single terminal for users."""
 
-    def P(self, *args):
-        P(self.name,*args)
-        pass
+        def __init__(self, obj, env, resource, t=25, mu=20, name=""):
+            self.obj = obj
+            self.env = env
+            self.resource = resource
+            self.action = env.process(self.run())
+            self.t = t
+            self.mu = mu
+            self.name = name
+            pass
 
-    def run(self):
-        """Running process."""
-        while True:
-            # Generate problem
-            st = self.env.now
-            yield self.env.process(self.gen_problem())
-            gen_times.append(self.env.now - st)
+        def P(self, *args):
+            P(self.name,*args)
+            pass
 
-            # Solution
-            st = self.env.now
-            yield self.env.process(self.request_solve())
-            solve_times.append(self.env.now - st)
+        def run(self):
+            """Running process."""
+            while True:
+                # Generate problem
+                st = self.env.now
+                yield self.env.process(self.gen_problem())
+                self.obj.gen_times.append(self.env.now - st)
+
+                # Solution
+                st = self.env.now
+                yield self.env.process(self.request_solve())
+                self.obj.solve_times.append(self.env.now - st)
+                pass
+            pass
+
+        def gen_problem(self):
+            """Simulate generating a problem."""
+        
+            self.P("Generating problem @ %f.02" % self.env.now)
+            duration = random.expovariate(1.0/self.t)
+            yield self.env.timeout(duration) # Generate next problem
+            pass
+
+        def request_solve(self):
+            """Simulate a request to solve the problem."""
+        
+            with self.resource.request() as req:
+                self.P("Requesting terminal access @ %f.02" % self.env.now)
+                yield req # Request access
+
+                self.P("Requesting solution @ %f.02" % self.env.now)
+                duration = random.expovariate(1.0/self.mu)
+                yield self.env.timeout(duration) # Do calculation
             pass
         pass
 
-    def gen_problem(self):
-        """Simulate generating a problem."""
-        
-        self.P("Generating problem @ %f.02" % self.env.now)
-        duration = random.expovariate(1.0/self.t)
-        yield self.env.timeout(duration) # Generate next problem
+    class RemoteTerminal(Terminal):
+        """Represents a remote user."""
+
+        def __init__(self, obj, env, resource, t = 25, mu = 20, a=10, b=30, name=""):
+            super().__init__(obj, env, resource, t=t, mu=mu, name=name)
+            self.a = a
+            self.b = b
+            pass
+
+        def request_solve(self):
+            """Simulate a request to solve the problem.
+            Note that extra time is required to transfer data."""
+
+            with self.resource.request() as req:
+                self.P("Requesting terminal access @ %f.02" % self.env.now)
+                yield req # Request access
+
+                self.P("Moving data @ %f.02" % self.env.now)
+                transf_time = random.randint(self.a, self.b)
+                yield self.env.timeout(transf_time) # Transfer data
+
+                self.P("Requesting solution @ %f.02" % self.env.now)
+                duration = random.expovariate(1.0/self.mu)
+                yield self.env.timeout(duration) # Do calculation
+            pass
         pass
 
-    def request_solve(self):
-        """Simulate a request to solve the problem."""
-        
-        with self.resource.request() as req:
-            self.P("Requesting terminal access @ %f.02" % self.env.now)
-            yield req # Request access
-
-            self.P("Requesting solution @ %f.02" % self.env.now)
-            duration = random.expovariate(1.0/self.mu)
-            yield self.env.timeout(duration) # Do calculation
-        pass
-    pass
-
-class RemoteTerminal(Terminal):
-    
-    def __init__(self, env, resource, t = 25, mu = 20, a=10, b=30, name=""):
-        super().__init__(env, resource, t=t, mu=mu, name=name)
+    def __init__(self, t = 25, mu = 20, a=10, b=30, n_remote=3, n_processors=2):
+        # Options
         self.a = a
         self.b = b
+        self.t = t
+        self.mu = mu
+        self.n_remote = 3
+        self.n_processors = n_processors
+
+        # Save data
+        self.gen_times = []
+        self.solve_times = []
         pass
 
-    def request_solve(self):
-        """Simulate a request to solve the problem.
-        Note that extra time is required to transfer data."""
+    def run(self, T=3600):
+        """Runs a simulation for T seconds."""
+        env = simpy.Environment()
+        procs = simpy.Resource(env, capacity=self.n_processors)
+        
+        home_term = Lab18.Terminal(self, env, procs, name="home  ")
+        remote_terms = [Lab18.RemoteTerminal(self, env, procs, name="sess%d " % i) for i in range(3)]
 
-        with self.resource.request() as req:
-            self.P("Requesting terminal access @ %f.02" % self.env.now)
-            yield req # Request access
+        env.run(until=3600) # Run for T = 60*60 seconds = 60 minutes
+        return self
 
-            self.P("Moving data @ %f.02" % self.env.now)
-            transf_time = random.randint(self.a, self.b)
-            yield self.env.timeout(transf_time) # Transfer data
+    def output(self):
+        plt.title("Problem generation times.")
+        plt.hist(self.gen_times)
+        plt.show()
 
-            self.P("Requesting solution @ %f.02" % self.env.now)
-            duration = random.expovariate(1.0/self.mu)
-            yield self.env.timeout(duration) # Do calculation
-        pass
+        plt.title("Problem solution times (incl. waiting).")
+        plt.hist(self.solve_times)
+        plt.show()
+        return self
     pass
 
-env = simpy.Environment()
-procs = simpy.Resource(env, capacity=2)
-home_term = Terminal(env, procs, name="home  ")
-remote_terms = [RemoteTerminal(env, procs, name="sess%d " % i) for i in range(3)]
-env.run(until=3600) # Run for T = 60*60 seconds = 60 minutes
-
-gen_times = np.array(gen_times)
-solve_times = np.array(solve_times)
-
-plt.title("Problem generation times.")
-plt.hist(gen_times)
-plt.show()
-
-plt.title("Problem solution times (incl. waiting).")
-plt.hist(solve_times)
-plt.show()
-
+l18 = Lab18().run(3600)
+l18.output()
 
 z37 = """
  ЗАДАНИЕ 37. Моделирование процесса функционирования участка контроля 
@@ -132,3 +156,4 @@ z37 = """
 Разработать GPSSV – модель для анализа процесса функционирования участка контроля в течение смены, т.е. 8 часов. 
 Первоначальный перечень экспериментов: a=5, δ=2, b=9, β=3, c=30, γ=10.
 """
+
